4a57004824ae79416fe97105b85f30bb3e8fc8ac
Amit Kapoor <amitkaps@gmail.com>
2017-09-10 09:52:04 +0530

    plot improvement

--- modelvis.py
+++ modelvis.py
@@ -1,19 +1,19 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import tree
+from tree import DecisionTreeClassifier
 
-
-def plot_classifier(clf, X, y):
-    x1_min, x1_max = X.iloc[:,0].min(), X.iloc[:,0].max()
-    x2_min, x2_max = X.iloc[:,1].min(), X.iloc[:,1].max()
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, (x1_max - x1_min)/100),
-            np.arange(x2_min, x2_max, (x2_max - x2_min)/100))
-
-    Z = clf.predict_proba(np.c_[xx1.ravel(), xx2.ravel()])[:,0]
-    Z = Z.reshape(xx1.shape)
-    cs = plt.contourf(xx1, xx2, Z, cmap=plt.cm.magma, alpha = 0.5)
-    plt.scatter(x = X.iloc[:,0], y = X.iloc[:,1], c = y, s = 50, alpha = 0.3)
+def plot_classifier_2d(clf, data, target):
+    x_min, x_max = data.iloc[:,0].min(), data.iloc[:,0].max()
+    y_min, y_max = data.iloc[:,1].min(), data.iloc[:,1].max()
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, (x_max - x_min)/100), np.arange(y_min, y_max, (y_max - y_min)/100))
+    Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:,0]
+    Z = Z.reshape(xx.shape)
+    cs = plt.contourf(xx, yy, Z, cmap=plt.cm.magma, alpha = 0.5)
+    plt.scatter(x = data.iloc[:,0], y = data.iloc[:,1], c = target, s = 50, alpha = 0.3)
     plt.colorbar(cs)
 
 if __name__ == "__main__":
-    main()
+    print("welcome to model visualisation")
+
